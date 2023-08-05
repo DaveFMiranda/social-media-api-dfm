2d60084b72660ff386df93b1b33e8dc8169c81a5
Noob Evad <davefrankmiranda@gmail.com>
2023-08-05 10:16:38 -0500

    added thoughtController

--- main/models/User.js
+++ main/models/User.js
@@ -2,7 +2,7 @@ const { Schema, Types, model } = require('mongoose');
 // DAVE SOMETHING MIGHT BE WRONG WITH THIS LINE
 const thoughtSchema = require('./Thought');
 
-// Schema to create Student model
+// Schema to create User model
 const userSchema = new Schema(
   {
     username: {
