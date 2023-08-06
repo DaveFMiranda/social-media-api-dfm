62413db9dd3d910dd29cd1888db0a8d982f458f5
Noob Evad <davefrankmiranda@gmail.com>
2023-08-06 11:52:11 -0500

    should seed 10 users with random thoughts

--- utils/seed.js
+++ utils/seed.js
@@ -1,56 +1,48 @@
-const connection = require('../config/connection');
-const { Course, Student } = require('../models');
-const { getRandomName, getRandomAssignments } = require('./data');
+const connection = require("../config/connection");
+const { User, Thought } = require("../models");
+const { getRandomName, getRandomEmail, getRandomThoughts } = require("./data");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
-    // Delete the collections if they exist
-    let courseCheck = await connection.db.listCollections({ name: 'courses' }).toArray();
-    if (courseCheck.length) {
-      await connection.dropCollection('courses');
-    }
+connection.once("open", async () => {
+  console.log("connected");
+  // Delete the collections if they exist
+  let thoughtsCheck = await connection.db
+    .listCollections({ name: "thoughts" })
+    .toArray();
+  if (thoughtsCheck.length) {
+    await connection.dropCollection("thoughts");
+  }
 
-    let studentsCheck = await connection.db.listCollections({ name: 'students' }).toArray();
-    if (studentsCheck.length) {
-      await connection.dropCollection('students');
-    }
+  let usersCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
+  if (usersCheck.length) {
+    await connection.dropCollection("users");
+  }
 
+  // Create empty array to hold the users
+  const users = [];
 
-  // Create empty array to hold the students
-  const students = [];
-
-  // Loop 20 times -- add students to the students array
-  for (let i = 0; i < 20; i++) {
+  // Loop 10 times -- add users to the users array
+  for (let i = 0; i < 10; i++) {
     // Get some random assignment objects using a helper function that we imported from ./data
-    const assignments = getRandomAssignments(20);
-
-    const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const last = fullName.split(' ')[1];
-    const github = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+    const username = getRandomName();
+    const email = getRandomEmail();
+    const thoughts = getRandomThoughts(3);
 
     students.push({
-      first,
-      last,
-      github,
-      assignments,
+      username,
+      email,
+      thoughts,
     });
   }
 
   // Add students to the collection and await the results
-  await Student.collection.insertMany(students);
-
-  // Add courses to the collection and await the results
-  await Course.collection.insertOne({
-    courseName: 'UCLA',
-    inPerson: false,
-    students: [...students],
-  });
+  await User.collection.insertMany(users);
 
   // Log out the seed data to indicate what should appear in the database
-  console.table(students);
-  console.info('Seeding complete! 🌱');
+  console.table(users);
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
